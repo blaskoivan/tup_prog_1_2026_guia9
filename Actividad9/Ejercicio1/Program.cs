d2bb486b56e0dfeab03850c73193950f1535e16a
blaskoivan <blaskocuenta@gmail.com>
2026-05-21 10:53:15 -0300

    incompleto2

--- Actividad9/Ejercicio1/Program.cs
+++ Actividad9/Ejercicio1/Program.cs
@@ -2,9 +2,8 @@
 
 namespace Ejercicio1
 {
-    internal class Program
+    internal class NumericService
     {
-        Program p= new Program();
         public int acum = 0;
         public int cont = 0;
         public void RegistrarNumero(int numero)
@@ -26,48 +25,57 @@ namespace Ejercicio1
             }
             return prom;
         }
-        
-        public void MostrarPantallaMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Titulo menu");
-            Console.WriteLine("1. Ingrese un numero");
-            Console.WriteLine("2. Mostrar cant acum");
-            Console.WriteLine("3. Mostrar cant ingresados");
-            Console.WriteLine("4. Mostrar Promedio");
-        }
-        public void MostrarPantallaSolicitarNumero()
-        { 
+        class Program {
 
-            Console.Clear();
-            Console.WriteLine("Ingrese un numero");
-            int numero=Convert.ToInt32(Console.ReadLine());
-            p.RegistrarNumero(numero);
-            Console.WriteLine("Presione una tecla para continuar");
-        }
-        public void MostrarPantallaAcumulado()
-        {
-            Console.Clear();
-            Console.WriteLine($"El total acumulado es: {acum}");
-            Console.WriteLine("Presione una tecla para continuar");
-            Console.ReadKey();
-        }
-        public void MostrarCantidadNumerosIngresados()
-        {
-            Console.Clear();
-            Console.WriteLine($"La cantidad de numeros ingresados es: {cont}");
-            Console.WriteLine("Presione una tecla para continuar");
-            Console.ReadKey();
-        }
-        public void MostrarPromedio()
-        {
+            static NumericService p= new NumericService();
+            public void MostrarPantallaMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Titulo menu");
+                Console.WriteLine("1. Ingrese un numero");
+                Console.WriteLine("2. Mostrar cant acum");
+                Console.WriteLine("3. Mostrar cant ingresados");
+                Console.WriteLine("4. Mostrar Promedio");
+            }
+            public void MostrarPantallaSolicitarNumero()
+            {
 
-            Console.WriteLine($"El promedio es {p.CalcularPromedio()}");
-        }
+                Console.Clear();
+                Console.WriteLine("Ingrese un numero");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                p.RegistrarNumero(numero);
+                Console.WriteLine("Presione una tecla para continuar");
+            }
+            public void MostrarPantallaAcumulado()
+            {
+                Console.Clear();
+                Console.WriteLine($"El total acumulado es: {p.acum}");
+                Console.WriteLine("Presione una tecla para continuar");
+                Console.ReadKey();
+            }
+            public void MostrarCantidadNumerosIngresados()
+            {
+                Console.Clear();
+                Console.WriteLine($"La cantidad de numeros ingresados es: {p.cont}");
+                Console.WriteLine("Presione una tecla para continuar");
+                Console.ReadKey();
+            }
+            public void MostrarPromedio()
+            {
 
+                Console.WriteLine($"El promedio es {p.CalcularPromedio()}");
+            }
+        }
             static void Main(string[] args)
         {
+            int opcion;
+            do
+            {
+                opcion=
+                
+                
 
+            } while (opcion > 0);
 
         }
     }
